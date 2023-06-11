2dc880e8930dfccaf6dfb11e249a916c75134b40
Tartclient <henry.ariasguerrero@gmail.com>
2023-06-11 16:30:39 -0700

    added JS link and modified it

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+//let div = document.createElement('div');
+
+//let textNode = document.createTextNode('Here I am');
+
+// 1. Create <div> element
+let div = document.createElement('div');
+
+// 2. Set its class to "alert"
+div.className = "alert";
+
+// 3. Fill it with the content
+div.innerHTML = "<strong>Hi there!</strong> You've read an important message.";
